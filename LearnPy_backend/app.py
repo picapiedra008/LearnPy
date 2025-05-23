192b92c839149d0c17610962ecc98fcee1587199
jhon alan <jhonalanapazacondori@gmail.com>
2025-05-23 01:18:01 -0500

    se trabayo en las apis de usuario,leccion,material,ejercicios

--- LearnPy_backend/app.py
+++ LearnPy_backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from src.routers import user, exercise, lesson
+from src.routers import user, exercise, lesson, material
 app = Flask(__name__)
 
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024
@@ -17,7 +17,7 @@ CORS(app, resources={
 app.register_blueprint(user.main, url_prefix='/user')
 app.register_blueprint(lesson.main, url_prefix='/lesson')
 app.register_blueprint(exercise.main, url_prefix='/exercise')
-
+app.register_blueprint(material.main, url_prefix='/material')
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
