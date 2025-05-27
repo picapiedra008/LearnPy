66f5c62db1318e7055e774da5bac906197675fbf
Johel-code <jbsch16@gmail.com>
2025-05-27 12:11:05 -0400

    fix: refactorizando obtener lecciones estaticas

--- LearnPy_frontend_administrator/src/App.jsx
+++ LearnPy_frontend_administrator/src/App.jsx
@@ -2,32 +2,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute'; 
-import Home from './pages/Home/Home'; 
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import Register from './pages/Register/Register'; 
+import Register from './pages/Register/Register';
 import Usuarios from './pages/ListarUsuarios/usuarios';
 import EditUser from './pages/EditUser/EditUser';
+import Lecciones from './pages/ListaLecciones/lecciones';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/login" element={<Login />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route
             path="/register"
-            element={<PrivateRoute element={<Register />} />} 
+            element={<PrivateRoute element={<Register />} />}
           />
           <Route
             path="/listUsers"
-            element={<PrivateRoute element={<Usuarios />} />} 
+            element={<PrivateRoute element={<Usuarios />} />}
           />
-         
+
           <Route
             path="/editar/:id"
-            element={<PrivateRoute element={<EditUser />} />} 
+            element={<PrivateRoute element={<EditUser />} />}
+          />
+          <Route
+            path="/lecciones"
+            element={<Lecciones />}
           />
         </Routes>
       </Router>
