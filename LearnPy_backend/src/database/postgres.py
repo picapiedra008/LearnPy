ff4ad209351b1e1c7ac0e768dafbf062103d09a7
Romulo-Tolemaico <tolemaicor@gmail.com>
2025-05-27 22:26:57 -0400

    Cambiando credencial

--- LearnPy_backend/src/database/postgres.py
+++ LearnPy_backend/src/database/postgres.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        dbname="learnpy_db",
-        user="learnpy_db_user",
-        password="UWoCM3iaAGR3x4Eagh6CWbQvXQpfupZl",
-        host="dpg-d04pcauuk2gs73dttt4g-a.oregon-postgres.render.com",
+        dbname="learnpy_db_n5ko",
+        user="learnpy_db_n5ko_user",
+        password="gd8BR4uxeSQ6gBlp9OYm1AwegDGScUyX",
+        host="dpg-d0r75aadbo4c73a34vag-a.oregon-postgres.render.com",
         port="5432"
     )
