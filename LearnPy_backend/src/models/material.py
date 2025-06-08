571a8db1df59a2e48e8e835a23aee81c3a2491bd
Johel-code <jbsch16@gmail.com>
2025-06-07 23:29:42 -0400

    feat: Agregando funcionalidad para crear lesson

--- LearnPy_backend/src/models/material.py
+++ LearnPy_backend/src/models/material.py
@@ -5,7 +5,7 @@ import os
 class Material():
 
     @classmethod
-    def create_material(self, lesson_code: int, file, material_type_code: int, material_name: str):
+    def create_material(self, topic_code: int, file, material_type_code: int, material_name: str):
         try:
             # Subir archivo a Google Drive
             if file.filename == '':
@@ -26,7 +26,42 @@ class Material():
 
             cur.execute(
                 "SELECT create_material(%s, %s, %s, %s)",
-                (lesson_code, material_type_code, material_name, file_id)
+                (topic_code, material_type_code, material_name, file_id)
+            )
+            new_material_code = cur.fetchone()[0]
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            return {"material_code": new_material_code, "message": "Material creado correctamente"}, 201
+
+        except Exception as e:
+
+            return {"error": str(e)}, 500
+
+    @classmethod
+    def create_material_of_exercise(self, exercise_code: int, file, material_type_code: int, material_name: str):
+        try:
+            # Subir archivo a Google Drive
+            if file.filename == '':
+                return {'error': 'No selected file'}, 400
+
+            file_path = f"./{file.filename}"
+            file.save(file_path)
+
+            # Subir a Google Drive            
+            uploaded_file = upload_file_to_drive(file_path, file.filename, file.mimetype)
+            file_id = uploaded_file.get('id')
+
+            # Eliminar el archivo temporal después de la subida
+            os.remove(file_path)
+
+            conn = get_connection()
+            cur = conn.cursor()
+
+            cur.execute(
+                "SELECT create_material(%s, %s, %s, %s)",
+                (exercise_code, material_type_code, material_name, file_id)
             )
             new_material_code = cur.fetchone()[0]
             conn.commit()
