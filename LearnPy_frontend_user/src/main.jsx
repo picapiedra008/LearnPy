d5553d0f5cf24b91e413bf66cbb3f41506ccb284
victor-hugo12 <201801617@est.umss.edu>
2025-06-20 00:32:59 -0400

    feat: agregar compilador funcional con validación de ejercicios

--- LearnPy_frontend_user/src/main.jsx
+++ LearnPy_frontend_user/src/main.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Componentes principales
 import App from "./App";
-import CrearLecciones from "./pages/crearLecciones";
-import Ejercicios from "./pages/ejercicios";
-import LessonList from "./pages/LessonList";
-import Compilador from "./pages/Compilador/compilador";
-import EjerciciosEstudiante from "./pages/ejerciciosEstudiante";
 import "./index.css";
+
+// Páginas
+import ListarLecciones from "./pages/listarLecciones/listarLecciones";
+import CrearLeccion from "./pages/crearLeccion/crearLeccion"; // Para crear y editar
+import VerLeccion from "./pages/verLecciones/verLeccion";
+
+import Ejercicios from "./pages/ejercicios";
+import Compilador from "./pages/Compilador/compilador";
+
+import EjerciciosEstudiante from "./pages/ejerciciosEstudiante";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/listar" element={<LessonList />} />
-        <Route path="/crear-leccion" element={<CrearLecciones />} />
+
+        {/* Listar lecciones */}
+        <Route path="/listar" element={<ListarLecciones />} />
+
+
+        {/* Crear y editar lecciones */}
+        <Route path="/crear-leccion" element={<CrearLeccion />} />
+        <Route path="/editar-leccion/:id" element={<CrearLeccion />} />
+
+        {/* Ver lección como estudiante */}
+        <Route path="/curso/:id" element={<VerLeccion />} />
+
+        {/* Otros módulos */}
         <Route path="/ejercicios" element={<Ejercicios />} />
         <Route path="/compilador" element={<Compilador />} />
+        
         <Route path="/entorno" element={<EjerciciosEstudiante/>} />
       </Routes>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
